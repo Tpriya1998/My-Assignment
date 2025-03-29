406595a97079827c390b8f330650c578d76949f7
Tpriya1998 <priyatanu56@gmail.com>
2025-03-29 23:16:15 +0530

    Add files via upload
    Day1

---
+++ day1/Chrome.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version=91.0f;
+		String developer="Google";
+		boolean beta=false;
+		int releaseYear=2008;
+		char shortCutKey='C';
+		
+		System.out.println(version);
+		System.out.println(developer);
+		System.out.println(beta);
+		System.out.println(releaseYear);
+		System.out.println(shortCutKey);
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ day1/Name.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class Name {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Inderjit Kumar");
+		System.out.println("i am currently posted as pointsman at Vandlur Station");
+		// TODO Auto-generated method stub
+
+	}
+
+}
